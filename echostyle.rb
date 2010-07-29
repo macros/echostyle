963f20afc3879a08bf3d73699dd50a5e3bf298df
Jason Cook <jasonc@simpleideas.org>
2010-07-29 01:21:40 -0700

    Forgot to commit the actual code cleanup

--- echostyle.rb
+++ echostyle.rb
@@ -8,10 +8,9 @@ set :root, APP_ROOT
 
 post '/' do
   scss = request.body.read.to_s
-  default_options = Sass::Engine::DEFAULT_OPTIONS.dup
-  default_options.delete(:syntax)
-  options = {:syntax => :scss}.merge(default_options)
-  engine = Sass::Engine.new(scss, options)
-  root = engine.to_tree
-  root.render
+  engine = Sass::Engine.new(scss, :syntax => :scss, 
+                            :style => :compact,
+                            :load_paths => ['css'],
+                            :cache_location => 'cache')
+  engine.render
 end
